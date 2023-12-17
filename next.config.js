c093ecc035a397822154f8621fe68254f0c27601
Ben <ben@tcg-vault.com>
2023-12-17 12:58:22 +0100

    IPK-14 Config and css fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["assets-global.website-files.com"]
+    domains: ["assets-global.website-files.com"]
   }
 };
 
