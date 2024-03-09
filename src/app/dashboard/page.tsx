bbd8fe67eae66d4fbf3fde1674145ca5420c09fb
Ben <ben@tcg-vault.com>
2024-03-09 21:21:10 +0100

    feat: initial structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import React from "react";
 import { LuLayoutDashboard, LuBookCopy, LuSettings, LuCreditCard, LuGauge, LuUser2, LuBuilding } from "react-icons/lu";
 
@@ -62,11 +64,33 @@ function Dashboard() {
         </div>
       </aside>
       <main className="mr-8 mt-8 min-h-screen w-full rounded-[20px] border-2 border-border md:w-2/3 lg:w-3/4">
-        <div className="p-4">
-          <h1 className="m-4 font-medium text-muted-foreground md:text-2xl">Welcome back, Tim</h1>
+        <div className="flex flex-col items-center justify-start p-8 px-6">
+          <header className="flex w-full flex-row items-center justify-between">
+            <h1 className="m-4 font-medium text-muted-foreground md:text-2xl">Welcome back, Tim</h1>
+            <div className="flex items-center justify-between gap-x-6">
+              <Input className="w-[320px]" placeholder="Search"></Input>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                <AvatarFallback>TL</AvatarFallback>
+              </Avatar>
+            </div>
+          </header>
 
-          <div className="h-[250vh]"></div>
+          <div className="mt-8 flex h-[200px] w-full flex-col items-start justify-center gap-2 rounded-[20px] bg-primary pl-12">
+            <h2 className="text-3xl font-semibold">MeetMate Dashboard</h2>
+            <p className="text-sm">Create your appointments in minutes</p>
+            <Button className="mt-2" variant={"secondary"}>
+              Book now
+            </Button>
+          </div>
+
+          <div className="mt-8 flex h-[600px] w-full rounded-[20px] bg-subtle"></div>
+
+          <div className="h-[50vh]"></div>
         </div>
+        <footer className="flex h-8 w-full items-center justify-start rounded-[20px] bg-primary">
+          <p className="pl-4 text-sm font-medium text-background">MeetMate</p>
+        </footer>
       </main>
     </div>
   );
