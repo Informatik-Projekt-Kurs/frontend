a56152c04e66bd8aac90d7cbf8a48d3008455329
Ben Böckmann <boeckmannben@gmail.com>
2024-04-02 13:44:08 +0200

    fix: quick improvement env

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: process.env.BACKEND_DOMAIN + "/api/:path*"
+        destination: `${process.env.BACKEND_DOMAIN}/api/:path*`
       }
     ];
   }
