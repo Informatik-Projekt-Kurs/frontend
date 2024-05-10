cc3a37500a20a15fc184ae5f4c1138b9064590f4
Ben Böckmann <boeckmannben@gmail.com>
2024-05-10 13:20:35 +0200

    build: added syncfusion license

--- src/components/dashboard/scheduler/Scheduler.tsx
+++ src/components/dashboard/scheduler/Scheduler.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ScheduleComponent, ViewsDirective, ViewDirective, Inject, WorkWeek } from "@syncfusion/ej2-react-schedule";
 import "./scheduler.css";
+import { registerLicense } from "@syncfusion/ej2-base";
 
 function Scheduler() {
   const data = [
@@ -8,15 +9,17 @@ function Scheduler() {
       Id: 1,
       Subject: "Scrum Meeting",
       Location: "Office",
-      StartTime: new Date(2024, 5, 6, 9, 30),
-      EndTime: new Date(2024, 5, 6, 10, 30),
+      StartTime: new Date(2024, 4, 6, 9, 30),
+      EndTime: new Date(2024, 4, 6, 10, 30),
       RecurrenceRule: "FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1"
     }
   ];
   const eventSettings = { dataSource: data };
 
+  registerLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXpfcHRSR2BdVUVxW0E=");
+
   return (
-    <ScheduleComponent height="650px" eventSetings={eventSettings} showHeaderBar={false}>
+    <ScheduleComponent height="650px" eventSettings={eventSettings} showHeaderBar={false}>
       <ViewsDirective>
         <ViewDirective option="WorkWeek" />
       </ViewsDirective>
