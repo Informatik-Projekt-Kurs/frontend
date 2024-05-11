93f6bd601450a4e0e051f691acb507ebea8d6bc7
Ben <boeckmannben@gmail.com>
2024-05-11 09:42:21 +0200

    style: added appointment template with random colors

--- src/components/dashboard/scheduler/Scheduler.tsx
+++ src/components/dashboard/scheduler/Scheduler.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ScheduleComponent, ViewsDirective, ViewDirective, Inject, WorkWeek } from "@syncfusion/ej2-react-schedule";
-import "./scheduler.css";
+import "./scheduler.scss";
 import { registerLicense } from "@syncfusion/ej2-base";
 import { Button } from "@/components/ui/button";
 
@@ -12,7 +12,15 @@ function Scheduler() {
       Location: "Office",
       StartTime: new Date(2024, 4, 6, 14, 30),
       EndTime: new Date(2024, 4, 6, 15, 30),
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR;INTERVAL=1"
+      RecurrenceRule: ""
+    },
+    {
+      Id: 2,
+      Subject: "Scrum Meeting",
+      Location: "Office",
+      StartTime: new Date(2024, 4, 8, 17, 30),
+      EndTime: new Date(2024, 4, 8, 18, 30),
+      RecurrenceRule: ""
     }
   ];
   const eventSettings = { dataSource: data };
@@ -31,10 +39,17 @@ function Scheduler() {
     setCurrentDate(previousWeek);
   };
 
+  const predefinedColors = ["#6EE7B7", "#F87171", "#FACC15", "#3B82F6"];
+
   registerLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXpfcHRSR2BdVUVxW0E=");
 
   const onEventClick = (args: { cancel: boolean }) => {
     args.cancel = true;
+  };
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * predefinedColors.length);
+    return predefinedColors[randomIndex];
   };
 
   return (
@@ -60,6 +75,26 @@ function Scheduler() {
             startHour="13:00"
             endHour="19:00"
             timeScale={{ interval: 60, slotCount: 2 }}
+            eventTemplate={(props: { Subject: string; StartTime: Date; EndTime: Date }) => (
+              <div
+                className={"absolute z-10 ml-[-4px] size-full rounded-[12px] border-[2px] border-solid p-2"}
+                style={{
+                  borderColor: getRandomColor()
+                }}>
+                <h2 className={"font-bold"}>{props.Subject}</h2>
+                <div className={"mt-1 text-xs text-muted-foreground"}>
+                  {new Intl.DateTimeFormat("en-US", {
+                    hour: "numeric",
+                    minute: "numeric"
+                  }).format(props.StartTime)}{" "}
+                  -{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    hour: "numeric",
+                    minute: "numeric"
+                  }).format(props.EndTime)}
+                </div>
+              </div>
+            )}
           />
         </ViewsDirective>
         <Inject services={[WorkWeek]} />
