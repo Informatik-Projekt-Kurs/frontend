67a1bf19920ad8cad5d80306f2886784a6dc6b40
Ben Böckmann <boeckmannben@gmail.com>
2024-06-22 13:37:50 +0200

    fix: apollo-wrapper.tsx updated to send to correct endpoint

--- src/lib/graphql/apollo-wrapper.tsx
+++ src/lib/graphql/apollo-wrapper.tsx
@@ -1,18 +1,33 @@
 "use client";
 
+import { ApolloLink, HttpLink } from "@apollo/client";
 import {
   ApolloNextAppProvider,
+  NextSSRInMemoryCache,
   NextSSRApolloClient,
-  NextSSRInMemoryCache
+  SSRMultipartLink
 } from "@apollo/experimental-nextjs-app-support/ssr";
 
 function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "https://meetmate.bencodes.de/graphql",
+    fetchOptions: { cache: "no-store" }
+  });
+
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
-    uri: process.env.BACKEND_DOMAIN + "/graphql"
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true
+            }),
+            httpLink
+          ])
+        : httpLink
   });
 }
 
-export function ApolloWrapper({ children }: React.PropsWithChildren) {
+export function ApolloWrapper({ children }: { children: React.ReactNode }) {
   return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>;
 }
