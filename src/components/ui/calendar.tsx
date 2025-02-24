63505a217eccfcb91b4212af33eada78dc9d5434
Ben Böckmann <75972296+bencodes07@users.noreply.github.com>
2025-02-24 12:20:01 +0100

    fix: calendar error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -46,10 +45,7 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: R
         day_hidden: "invisible",
         ...classNames
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => <ChevronLeft className={cn("size-4", className)} {...props} />,
-        IconRight: ({ className, ...props }) => <ChevronRight className={cn("size-4", className)} {...props} />
-      }}
+      components={{}}
       {...props}
     />
   );
