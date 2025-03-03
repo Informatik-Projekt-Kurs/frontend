92dbda31b733a38ea3e4f52eedee8cd4fb51f2c8
Ben Böckmann <75972296+bencodes07@users.noreply.github.com>
2025-03-03 12:53:57 +0100

    style: tailwind bugs fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import "../assets/tailwind.css";
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import "../assets/globals.scss";
+
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
